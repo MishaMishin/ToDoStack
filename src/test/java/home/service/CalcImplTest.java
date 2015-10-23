40dc74d110e0b10accfe5ba1e61a75d5cf272c5b
Yuliy <uliy86@gmail.com>
2015-10-23 13:11:09 +0200

    Create new test
    Create new test

---
+++ src/test/java/home/service/CalcImplTest.java
@@ -0,0 +1,16 @@
+package home.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class CalcImplTest {
+
+    @Test
+    public void AnyName(){
+
+        System.out.println("anyText");
+    }
+
+}
