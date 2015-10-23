461098fcc7637ed6cac35f9c4a0fbc63c9876c45
Yuliy <uliy86@gmail.com>
2015-10-23 13:41:41 +0200

    Update
    Update

--- src/test/java/home/service/CalcImplTest.java
+++ src/test/java/home/service/CalcImplTest.java
@@ -26,6 +26,19 @@ public class CalcImplTest {
     */
     }
 
+    public void add_test3(){
+    /*
+    * Если а = 3 и б = -5, то результатом сложения будет число -2
+    */
+    }
+    @Test
+    public void add_test4(){
+    /*
+    * Если а = -5 и б = 5, то результатом сложения будет число 0
+    */
+    }
+
+
 
     @Test
     public void sub_test1(){
@@ -41,6 +54,19 @@ public class CalcImplTest {
     }
 
     @Test
+    public void sub_test3(){
+    /*
+    * Если а = -5 и б = -10, то результатом вычитания будет число -15
+    */
+    }
+    @Test
+    public void sub_test4(){
+    /*
+    * Если а = 0 и б = 5, то результатом вычитания будет число -5
+    */
+    }
+
+    @Test
     public void devide_test1(){
     /*
     * Если а = 6 и б = 3, то результатом деления будет число 2
@@ -52,6 +78,20 @@ public class CalcImplTest {
     * Если а = 12 и б = 3, то результатом деления будет число 4
     */
     }
+
+    @Test
+    public void devide_test3(){
+    /*
+    * Если а = 6 и б = 0, ошибка на 0 делить нельзя
+    */
+    }
+    @Test
+    public void devide_test4(){
+    /*
+    * Если а = -12 и б = 3, то результатом деления будет число -4
+    */
+    }
+
     @Test
     public void mult_test1(){
     /*
@@ -62,6 +102,19 @@ public class CalcImplTest {
     public void mult_test2(){
     /*
     * Если а = 12 и б = 3, то результатом умножения будет число 36
+    */
+    }
+
+    @Test
+    public void mult_test3(){
+    /*
+    * Если а = -6 и б = 3, то результатом умножения будет число -18
+    */
+    }
+    @Test
+    public void mult_test4(){
+    /*
+    * Если а = 12 и б = 0, то результатом умножения будет число 0
     */
     }
 
